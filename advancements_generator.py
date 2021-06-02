02fd565df22b23115beaec701b3b8040ec3cc47d
onnowhere <17817284+onnowhere@users.noreply.github.com>
2021-06-02 05:01:52 -0700

    Reword to 'Advancement Disabler Generator'

--- advancements_generator.py
+++ advancements_generator.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
         except:
             print("Stopped.")
     except:
-        print("Impossible Advancements Generator by Onnowhere")
+        print("Advancement Disabler Generator by Onnowhere")
         print("Source: https://github.com/onnowhere/advancement-disabler")
         print("----------------------------------------------")
         print("This generates two datapacks based on the input Minecraft jar file.")
